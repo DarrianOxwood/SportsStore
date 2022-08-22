69ada2aadfb2abc2e79b0bd015ffde153b4f05e3
Иван Ступко <DarrianCrashOxwood@yandex.ru>
2022-08-22 14:18:17 +0700

    Initial commit

---
+++ Infrastructure/UrlExtension.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtension
+    {
+        public static string PathAndQuery(this HttpRequest request) =>
+            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
+    }
+}
